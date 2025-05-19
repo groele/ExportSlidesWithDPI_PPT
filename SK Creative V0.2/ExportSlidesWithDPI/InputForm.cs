a21afff6f556e46645832cf27724acc2fec8733e
Shikun <gro_ele@163.com>
2025-05-19 21:08:33 +0800

    Update to version V0.3

---
+++ SK Creative V0.2/ExportSlidesWithDPI/InputForm.cs
@@ -0,0 +1,12 @@
+﻿namespace ExportSlidesWithDPIDoing
+{
+    internal class InputForm
+    {
+        private string v;
+
+        public InputForm(string v)
+        {
+            this.v = v;
+        }
+    }
+}
